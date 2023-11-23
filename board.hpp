0c3a4b494925752bd8dc6f1f2ef1f7c39662b801
Arnas <arnasvaic7@gmail.com>
2023-11-23 11:18:10 +0200

    rename file, change impl to support only square boards

--- board.hpp
+++ board.hpp
@@ -2,16 +2,28 @@
 
 #include <bit>
 #include <array>
+#include <vector>
 #include <cstdint>
 #include <iostream>
 #include <unordered_map>
 #include <unordered_set>
 
-template<std::size_t TWidth, std::size_t THeight>
+class VectorU64Hash {
+public:
+    size_t operator()(const std::vector<uint64_t>& vector) const {
+        std::hash<uint64_t> hasher;
+        size_t seed = 0;
+        for (const auto& i : vector) {
+            seed ^= hasher(i) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+        }
+        return seed;
+    }
+};
+
+template<std::size_t TSize>
 class Board
 {
-    static_assert(TWidth <= 8, "Width must be less or equal to 8");
-    static_assert(THeight <= 8, "Height must be less or equal to 8");
+    static_assert(TSize <= 8, "Side length must be less or equal to 8");
 
 private:
 
@@ -27,11 +39,9 @@ private:
 
 public:
 
-    constexpr static std::size_t Width = TWidth;
+    constexpr static std::size_t Size = TSize;
 
-    constexpr static std::size_t Height = THeight;
-
-    constexpr static std::size_t Total = TWidth * THeight;
+    constexpr static std::size_t Total = TSize * TSize;
 
 
     constexpr Board();
@@ -48,7 +58,7 @@ public:
     constexpr void next_gen();
 
     std::vector<uint64_t> find_cycle();
-    std::unordered_set<std::vector<uint64_t>> find_unique_cycles();
+    std::unordered_set<std::vector<uint64_t>, VectorU64Hash> find_unique_non_zero_cycles();
 
     constexpr static bool is_equivalent(uint64_t a, uint64_t b);
 
@@ -68,15 +78,15 @@ private:
     constexpr static std::array<uint64_t, Total> neighbour_mask_lookup = { []() constexpr 
     {
         std::array<uint64_t, Total> table {};
-        for (std::size_t row = 0; row < Height; ++row)
+        for (std::size_t row = 0; row < TSize; ++row)
         {
-            std::array<std::size_t, 3> y = { (Height + row - 1) % Height, row, (row + 1) % Height };
+            std::array<std::size_t, 3> y = { (TSize + row - 1) % TSize, row, (row + 1) % TSize };
 
-            for (std::size_t col = 0; col < Width; ++col)
+            for (std::size_t col = 0; col < TSize; ++col)
             {
-                std::array<std::size_t, 3> x = { (Width + col - 1) % Width, col, (col + 1) % Width };
+                std::array<std::size_t, 3> x = { (TSize + col - 1) % TSize, col, (col + 1) % TSize };
 
-                std::size_t index = to_index<Width>(row, col);
+                std::size_t index = to_index<TSize>(row, col);
 
                 for(std::size_t i = 0; i < 3; ++i)
                 {
@@ -85,7 +95,7 @@ private:
                         if(i == 1 && j == 1)
                             continue;
 
-                        set_bit<Width>(y[i], x[j], table[index]);
+                        set_bit<TSize>(y[i], x[j], table[index]);
                     }
                 }
             }
@@ -95,41 +105,41 @@ private:
     
 };
 
-template<std::size_t Width, std::size_t Height>
-constexpr Board<Width, Height>::Board() 
+template<std::size_t TSize>
+constexpr Board<TSize>::Board() 
     : m_state(0), m_frame(0), m_visited()
 {
 
 }
 
-template<std::size_t Width, std::size_t Height>
-constexpr Board<Width, Height>::Board(uint64_t state)
+template<std::size_t TSize>
+constexpr Board<TSize>::Board(uint64_t state)
 {
     set(state);
 }
 
-template<std::size_t Width, std::size_t Height>
-constexpr void Board<Width, Height>::set(uint64_t state)
+template<std::size_t TSize>
+constexpr void Board<TSize>::set(uint64_t state)
 {
     m_state = state;
     m_frame = 0;
     m_visited.clear();
 }
 
-template<std::size_t Width, std::size_t Height>
-constexpr uint64_t Board<Width, Height>::get() const
+template<std::size_t TSize>
+constexpr uint64_t Board<TSize>::get() const
 {
     return m_state;
 }
 
-template<std::size_t Width, std::size_t Height>
-constexpr std::size_t Board<Width, Height>::get_frame() const
+template<std::size_t TSize>
+constexpr std::size_t Board<TSize>::get_frame() const
 {
     return m_frame;
 }
 
-template<std::size_t Width, std::size_t Height>
-constexpr void Board<Width, Height>::next_gen()
+template<std::size_t TSize>
+constexpr void Board<TSize>::next_gen()
 {
     uint64_t next = 0;
     for(size_t i = 0; i < Total; ++i)
@@ -142,8 +152,8 @@ constexpr void Board<Width, Height>::next_gen()
     m_state = next;
 }
 
-template<std::size_t Width, std::size_t Height>
-std::vector<uint64_t> Board<Width, Height>::find_cycle()
+template<std::size_t TSize>
+std::vector<uint64_t> Board<TSize>::find_cycle()
 {
     for(;;)
     {
@@ -168,11 +178,12 @@ std::vector<uint64_t> Board<Width, Height>::find_cycle()
     }
 }
 
-template<std::size_t TWidth, std::size_t THeight>
-std::unordered_set<std::vector<uint64_t>> Board<TWidth, THeight>::find_unique_cycles()
+template<std::size_t TSize>
+std::unordered_set<std::vector<uint64_t>, VectorU64Hash> Board<TSize>::find_unique_non_zero_cycles()
 {
-    std::unordered_set<std::vector<uint64_t>> cycles;
+    std::unordered_set<std::vector<uint64_t>, VectorU64Hash> cycles;
     uint64_t s = 0;
+
     for(uint64_t i = 0; i < (1 << Total); ++i)
     {
         set(i);
@@ -188,7 +199,7 @@ std::unordered_set<std::vector<uint64_t>> Board<TWidth, THeight>::find_unique_cy
 
         // Check if there exists another element b such that P(a, b) is satisfied
         for (std::vector<uint64_t> other : cycles) {
-            if (Board<w,h>::is_equivalent(current, other) && other != current) {
+            if (are_cycles_equivalent(current, other) && other != current) {
                 predicateSatisfied = true;
                 break;
             }
@@ -205,19 +216,22 @@ std::unordered_set<std::vector<uint64_t>> Board<TWidth, THeight>::find_unique_cy
     return cycles;
 }
 
-bool are_cycles_equivalent(const std::vector<uint64_t> &lhs, const std::vector<uint64_t> &rhs)
+template<std::size_t TSize>
+constexpr bool Board<TSize>::are_cycles_equivalent(
+    const std::vector<uint64_t> &lhs, 
+    const std::vector<uint64_t> &rhs)
 {
     for (const auto &elem : rhs)
     {
-        if(lhs[0])
+        if(is_equivalent(lhs[0], elem))
+            return true;
     }
+    return false;
 }
 
-template<std::size_t Width, std::size_t Height>
-std::ostream& operator<<(std::ostream& os, const Board<Width, Height>& board)
+template<std::size_t TSize>
+std::ostream& operator<<(std::ostream& os, const Board<TSize>& board)
 {
-    //os << "frame: " << board.get_frame() << '\n';
-
     uint64_t state = board.get();
 
     for(int i = 0; i < board.Total; ++i)
@@ -225,30 +239,40 @@ std::ostream& operator<<(std::ostream& os, const Board<Width, Height>& board)
         char c = (state & (1 << i)) == 0 ? '.' : '#';
         os << c << ' ';
 
-        if((1 + i) % Width == 0)
+        if((1 + i) % TSize == 0)
             os << '\n';
     }
 
     return os;
 }
 
-template<std::size_t Width>
-constexpr std::size_t to_index(std::size_t row, std::size_t col)
+template<std::size_t TSize>
+std::ostream& operator<<(std::ostream& os, const std::vector<Board<TSize>>& cycle)
 {
-    return col + row * Width;
+    os << "Cycle:\n";
+    for(const auto& frame : cycle) {
+        os << frame << '\n';
+    }
+    return os;
 }
 
-template<std::size_t Width>
+template<std::size_t TSize>
+constexpr std::size_t to_index(std::size_t row, std::size_t col)
+{
+    return col + row * TSize;
+}
+
+template<std::size_t TSize>
 constexpr void set_bit(
     std::size_t row, 
     std::size_t col, 
     uint64_t &input)
 {
-    input |= (1 << to_index<Width>(row, col));
+    input |= (1 << to_index<TSize>(row, col));
 }
 
-template<std::size_t Width, std::size_t Height>
-constexpr bool Board<Width, Height>::is_equivalent(uint64_t a, uint64_t b)
+template<std::size_t TSize>
+constexpr bool Board<TSize>::is_equivalent(uint64_t a, uint64_t b)
 {
     uint64_t h = flip<true, false>(b);
     uint64_t v = flip<false, true>(b);
@@ -260,39 +284,39 @@ constexpr bool Board<Width, Height>::is_equivalent(uint64_t a, uint64_t b)
         is_translated(a, hv);
 }
 
-template<std::size_t Width, std::size_t Height>
+template<std::size_t TSize>
 template<bool horz, bool vert>
-constexpr uint64_t Board<Width, Height>::flip(uint64_t state)
+constexpr uint64_t Board<TSize>::flip(uint64_t state)
 {
     uint64_t result = 0;
-    for(std::size_t row = 0; row < Height; ++row)
+    for(std::size_t row = 0; row < TSize; ++row)
     {
-        for(std::size_t col = 0; col < Width; ++col)
+        for(std::size_t col = 0; col < TSize; ++col)
         {
-            std::size_t old_index = to_index<Width>(row, col);
+            std::size_t old_index = to_index<TSize>(row, col);
             bool alive = state & (1 << old_index);
 
             size_t new_row = row;
             if (horz)
-                new_row = Height - row - 1;
+                new_row = TSize - row - 1;
 
             size_t new_col = col;
             if (vert)
-                new_col = Width - col - 1;
+                new_col = TSize - col - 1;
 
-            std::size_t new_index = to_index<Width>(new_row, new_col);
+            std::size_t new_index = to_index<TSize>(new_row, new_col);
             result |= (alive << new_index);
         }
     }
     return result;
 }
 
-template<std::size_t Width, std::size_t Height>
-constexpr bool Board<Width, Height>::is_translated(uint64_t a, uint64_t b)
+template<std::size_t TSize>
+constexpr bool Board<TSize>::is_translated(uint64_t a, uint64_t b)
 {
-    for(size_t row = 0; row < Height; ++row)
+    for(size_t row = 0; row < TSize; ++row)
     {
-        for(size_t col = 0; col < Width; ++col)
+        for(size_t col = 0; col < TSize; ++col)
         {
             if(translate(row, col, b) == a)
                 return true;
@@ -301,23 +325,23 @@ constexpr bool Board<Width, Height>::is_translated(uint64_t a, uint64_t b)
     return false;
 }
 
-template<std::size_t Width, std::size_t Height>
-constexpr uint64_t Board<Width, Height>::translate(
+template<std::size_t TSize>
+constexpr uint64_t Board<TSize>::translate(
     std::size_t row_offset, 
     std::size_t col_offset, 
     uint64_t state)
 {
     uint64_t result = 0;
 
-    for(std::size_t row = 0; row < Height; ++row)
+    for(std::size_t row = 0; row < TSize; ++row)
     {
-        for(std::size_t col = 0; col < Width; ++col)
+        for(std::size_t col = 0; col < TSize; ++col)
         {
-            std::size_t old_index = to_index<Width>(row, col);
+            std::size_t old_index = to_index<TSize>(row, col);
             bool alive = state & (1 << old_index);
-            std::size_t new_index = to_index<Width>(
-                (row + row_offset) % Height,
-                (col + col_offset) % Width);
+            std::size_t new_index = to_index<TSize>(
+                (row + row_offset) % TSize,
+                (col + col_offset) % TSize);
             result |= (alive << new_index);
         }
     }
--- main.cpp
+++ main.cpp
@@ -7,55 +7,33 @@
 #include <unordered_set>
 #include <bit>
 #include <bitset>
-#include <board.h>
+#include <board.hpp>
 
 using namespace std;
+using namespace chrono;
 
 int main(int argc, char** argv)
 {
-    constexpr size_t w = 4, h = 4;
-    Board<w,h> board;
+    constexpr size_t len = 4;
+    Board<len> board;
     
-    auto start = chrono::steady_clock::now();
-    auto cycles = board.find_cycles();
-    auto end = chrono::steady_clock::now();
-    auto ms = chrono::duration <double, milli> (end - start).count();
+    auto start = steady_clock::now();
+    auto cycles = board.find_unique_non_zero_cycles();
+    auto end = steady_clock::now();
+    auto ms = duration<double, milli>(end - start).count();
     
-    cout << "Program took: " << ms << " ms" << endl;
-    cout << "Board size: " << board.Width << " x " << board.Height << '\n'; 
-    cout << "Total configurations searched: " << (1 << (board.Total)) << endl;
+    cout << "total cycles:" << cycles.size() << '\n';
 
-    for (const auto& [period, count] : frequencies)
-        cout << count << " configurations converge to a cycle of " << period << " frames\n";
-
-    for (auto it = stable.begin(); it != stable.end();) {
-        uint64_t currentElement = *it;
-        bool predicateSatisfied = false;
-
-        // Check if there exists another element b such that P(a, b) is satisfied
-        for (uint64_t b : stable) {
-            if (Board<w,h>::is_equivalent(currentElement, b) && b != currentElement) {
-                predicateSatisfied = true;
-                break;
-            }
-        }
-
-        // If the predicate is satisfied, erase the current element
-        if (predicateSatisfied) {
-            it = stable.erase(it);
-        } else {
-            ++it;
-        }
-    }
-    
-    cout << "Unique 1-period states: " << stable.size() << '\n';
-    Board<w,h> temp;
-    for (const auto& state : stable)
+    Board<len> temp;
+    for (const auto& cycle : cycles)
     {
-        temp.set(state);
-        cout << bitset<temp.Total>(state) << '\n';
-        cout << temp << '\n';
-    }
+        cout << "Cycle\n";
 
-    //cout << BoardCompare<4, 4>()(0b0010000110000100, 0b0010010010000001) << '\n';
+        for(size_t j = 0; j < cycle.size(); ++j)
+        {
+            temp.set(cycle[j]);
+            //cout << bitset<temp.Total>(state) << '\n';
+            cout << temp << '\n';
+        }
+    }
 }
