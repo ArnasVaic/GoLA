4cef85f0f8ed8930843291f52c3316287b06c810
Arnas Vaicekauskas <arnasvaic7@gmail.com>
2024-01-13 16:02:14 +0200

    Set null space index to 0

--- main.cpp
+++ main.cpp
@@ -84,10 +84,9 @@ CycleSet<N> search_square_orbit()
     return cache;
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
 
-    constexpr size_t N = 6;
+    constexpr size_t N = 8;
 
     auto start = std::chrono::steady_clock::now();
     auto cycles = search_square_orbit<N>();
@@ -112,21 +111,38 @@ int main(int argc, char** argv)
     string filename = "s" + to_string(N) + "-" + to_string(dis(gen)) + ".txt";
     ofstream os(filename);
 
-    if(!os.is_open())
-    {
+    if (!os.is_open()) {
         cout << "Could not output input file: " << filename << '\n';
         return 0;
     }
 
-    unordered_map<Cycle<N>, size_t, Cycle<N>::Hash, Cycle<N>::Equal> cycle_indices;
-    size_t cycle_index_assignment_index = 0;
-    for(const auto &cycle : cycles)
-        cycle_indices[cycle] = cycle_index_assignment_index++;
+    // place empty space in the front
+    auto null_cycle = find_if(cycles.begin(), cycles.end(), [](Cycle<N> const &cycle) {
+        return cycle.frames().contains(0);
+    });
 
-    for (const auto &cycle : cycles)
-    {
+    unordered_map<Cycle<N>, size_t, Cycle<N>::Hash, Cycle<N>::Equal> cycle_indices;
+
+    cycle_indices[*null_cycle] = 0;
+
+    size_t cycle_index_assignment_index = 1;
+    for (const auto &cycle: cycles) {
+        if (Cycle<N>::Equal()(cycle, *null_cycle))
+            continue;
+
+        cycle_indices[cycle] = cycle_index_assignment_index++;
+    }
+
+    /// Cycles are stored in a vector in an order specified by cycle_indices
+    vector<Cycle<N>> cycles_vec(cycles.size(), *null_cycle);
+    for (auto const &c: cycles) {
+        cycles_vec[cycle_indices[c]] = c;
+    }
+
+
+    for (const auto &cycle: cycles) {
         os << "[T = " << cycle.frames().size() << ", id: " << cycle_indices[cycle] << "]\n";
-        for(const auto &frame : cycle.frames())
+        for (const auto &frame: cycle.frames())
             os << frame.get() << ' ';
         os << '\n' << cycle << '\n';
     }
@@ -134,44 +150,86 @@ int main(int argc, char** argv)
     unordered_map<Frame<N>, size_t, Frame<N>::Hash> visited_frames;
     vector<Frame<N>> cycle_frames;
 
-    MatrixXd matrix(cycles.size(), cycles.size());
+    MatrixXd matrix_divided(cycles.size(), cycles.size());
+    matrix_divided.setZero();
+
+    MatrixXi matrix(cycles.size(), cycles.size());
     matrix.setZero();
 
-    for(const auto& cycle : cycles)
-    {
+    for (const auto &cycle: cycles) {
         unordered_map<Cycle<N>, size_t, Cycle<N>::Hash, Cycle<N>::Equal> dest_cycles;
 
-        for(const auto& org_frame : cycle.frames())
-        {
-            for(size_t i = 0; i < Frame<N>::CellCount; ++i)
-            {
+        for (const auto &org_frame: cycle.frames()) {
+            for (size_t i = 0; i < Frame<N>::CellCount; ++i) {
                 Frame<N> frame = org_frame;
                 frame.toggle(i);
                 game.set(frame);
                 auto dest_cycle = game.find_cycle(visited_frames, cycle_frames);
 
-                if(dest_cycles.contains(dest_cycle))
+                if (dest_cycles.contains(dest_cycle))
                     dest_cycles[dest_cycle]++;
                 else
                     dest_cycles[dest_cycle] = 1;
             }
         }
 
-        for(const auto& [dest_cycle, dest_freq] : dest_cycles)
-        {
-            Eigen::Index row = cycle_indices[dest_cycle];
-            Eigen:Index col = cycle_indices[cycle];
-            auto m = static_cast<double>(dest_freq);
-            auto n = static_cast<double>(cycle.frames().size() * Frame<N>::CellCount);
-            matrix(row, col) += m / n;
+        for (const auto &[dest_cycle, dest_freq]: dest_cycles) {
+            const Index row = cycle_indices[dest_cycle];
+            const Index col = cycle_indices[cycle];
+            const auto n = static_cast<double>(cycle.frames().size() * Frame<N>::CellCount);
+            matrix(row, col) += dest_freq;// / n;
+            matrix_divided(row, col) += dest_freq / n;
         }
     }
-    matrix -= MatrixXd::Identity(matrix.rows(), matrix.cols());
-    matrix *= N * N;
+
+//    for (int i = 0; i < matrix.rows(); ++i)
+//    {
+//        for (int j = 0; j < matrix.cols(); ++j)
+//        {
+//            const auto n = cycles_vec[j].frames().size() * Frame<N>::CellCount;
+//            matrix(i, j) /= n;
+//        }
+//    }
+
+    //os << "Divide immediately\n";
+    //os << N * N * (matrix_divided - MatrixXd::Identity(matrix_divided.rows(), matrix_divided.cols())) << '\n';
+    //os << "Divide after\n";
+    //os << N * N * (matrix - MatrixXi::Identity(matrix_divided.rows(), matrix_divided.cols()))<< '\n';;
+
+    //matrix -= MatrixXi::Identity(matrix.rows(), matrix.cols());
+    //matrix *= N * N;
     //EigenSolver<MatrixXd> solver(matrix);
+
     //os << solver.eigenvalues() << endl;
-    os << matrix << "\n\n";
-    os << "Col Sums:\n" << matrix.colwise().sum() << "\n\n";
-    os << "Row Sums:\n" << matrix.rowwise().sum() << "\n";
+
+    for(Index i = 0; i < matrix.rows(); ++i)
+    {
+        for(Index j = 0; j < matrix.cols(); ++j)
+        {
+            const int n = cycles_vec[j].frames().size() * Frame<N>::CellCount;
+
+            // we're on the diagonal
+            if(i == j)
+                matrix(i, j) -= n;
+
+            // scale each entry
+            matrix(i, j) *= N * N;
+
+            if(0 == matrix(i, j) || 0 == matrix(i, j) % n)
+            {
+                os << setw(10) <<  matrix(i, j) / n;
+            }
+            else
+            {
+                int d = gcd(matrix(i, j), n);
+                char buf[20];
+                sprintf(buf, "%d/%d", matrix(i,j) / d, n / d);
+                os << setw(10) << buf;
+            }
+        }
+        os << '\n';
+    }
+    os << "Col Sums: " << matrix.colwise().sum() << '\n';
+    os << "Row Sums: " << matrix.rowwise().sum().transpose() << '\n';
     os.close();
 }
